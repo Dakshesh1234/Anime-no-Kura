533cf65521ddfa26b193bcc75fb8daa04ffa35de
Dakshesh1234 <95707585+Dakshesh1234@users.noreply.github.com>
2024-06-30 00:15:19 +0530

    Add files via upload

---
+++ app/page.js
@@ -0,0 +1,19 @@
+import LoadMore from '@/components/LoadMore'
+import fetchData from './fetchData'
+import RenderData from '@/components/RenderData'
+
+export default async function Home() {
+
+  const data = await fetchData(1)
+  return (
+    <main className="sm:p-16 py-16 px-8 flex flex-col gap-10">
+
+      <h2 className="text-3xl text-white font-bold">Explore Anime</h2>
+
+      <RenderData data={data}/>
+    
+      <LoadMore />
+
+    </main>
+  )
+}
